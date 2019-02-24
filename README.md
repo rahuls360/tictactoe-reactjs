1cd34f35d9da97b7f65baf0073eb4b5d8779cb65
Rahul Makhija <rahuls360@gmail.com>
2019-02-24 12:22:45 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# tictactoe-reactjs
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,126 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+class Square extends React.Component {
+  handleClick = () => {
+    this.props.updateState(this.props.index);
+  };
+
+  render() {
+    return (
+      <button className="square" onClick={this.handleClick}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    };
+  }
+
+  updateState = i => {
+    let squares = { ...this.state.squares };
+    let value = this.state.xIsNext ? "X" : "O";
+
+    console.log(this.calculateWinner(squares), squares[i]);
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = value;
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
+  };
+
+  renderSquare = i => {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        index={i}
+        updateState={this.updateState}
+        xIsNext={this.xIsNext}
+      />
+    );
+  };
+
+  calculateWinner = squares => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  render() {
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
